e0be4121f9ba503ffca621d7944de11fd9abb698
kjx63pro <kjx63pro@gmail.com>
2020-09-16 20:08:11 +0900

    Create & Respond With JSON Web Token

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const router = express.Router();
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const config = require('config');
 
 const User = require('../../models/User');
 
@@ -24,7 +26,22 @@ router.post('/', async (req, res) => {
     user.password = await bcrypt.hash(password, salt);
 
     await user.save();
-    res.json(user);
+
+    const payload = {
+      user: {
+        id: user._id,
+      },
+    };
+
+    jwt.sign(
+      payload,
+      config.get('jwtSecret'),
+      { expiresIn: 60 * 60 * 100 },
+      (err, token) => {
+        if (err) throw err;
+        res.json({ token });
+      }
+    );
   } catch (err) {
     console.error(err.message);
     res.status(500).json({ msg });
