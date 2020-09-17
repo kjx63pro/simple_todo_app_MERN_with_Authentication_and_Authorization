15a86220443b27b1c77a2ff143885ece04dbfdc6
kjx63pro <kjx63pro@gmail.com>
2020-09-17 11:42:54 +0900

    Auth Context & Initial State

--- client/src/App.js
+++ client/src/App.js
@@ -1,42 +1,45 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.css';
-import './App.css';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Todo from './components/Todo';
 import AddForm from './components/AddForm';
-
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-
-import TodoState from './context/todo/TodoState.js';
 import AppNavbar from './components/Navbar/AppNavbar';
 import About from './components/About';
 import Register from './components/Register';
 import Login from './components/Login';
 
+import TodoState from './context/todo/TodoState';
+import AuthState from './context/auth/AuthState';
+
+import './App.css';
+
 function App() {
   return (
-    <TodoState>
-      <Router>
-        <AppNavbar />
-        <div className='app'>
-          <Switch>
-            <Route path='/' exact>
-              <h2>
-                Simple Todo App with React Hooks{' '}
-                <span role='img' aria-label='rocket'>
-                  🚀
-                </span>{' '}
-              </h2>
-              <Todo />
-              <hr />
-              <AddForm />
-            </Route>
-            <Route path='/about' component={About}></Route>
-            <Route path='/register' component={Register}></Route>
-            <Route path='/login' component={Login}></Route>
-          </Switch>
-        </div>
-      </Router>
-    </TodoState>
+    <AuthState>
+      <TodoState>
+        <Router>
+          <AppNavbar />
+          <div className='app'>
+            <Switch>
+              <Route path='/' exact>
+                <h2>
+                  Simple Todo App with React Hooks{' '}
+                  <span role='img' aria-label='rocket'>
+                    🚀
+                  </span>{' '}
+                </h2>
+                <Todo />
+                <hr />
+                <AddForm />
+              </Route>
+              <Route path='/about' component={About}></Route>
+              <Route path='/register' component={Register}></Route>
+              <Route path='/login' component={Login}></Route>
+            </Switch>
+          </div>
+        </Router>
+      </TodoState>
+    </AuthState>
   );
 }
 
---
+++ client/src/context/auth/AuthState.js
@@ -0,0 +1,53 @@
+import React, { useReducer } from 'react';
+import axios from 'axios';
+import AuthContext from './AuthContext';
+import authReducer from './authReducer';
+
+import {
+  REGISTER_SUCCESS,
+  REGUSTER_FAIL,
+  USER_LOADED,
+  AUTH_ERROR,
+  LOGIN_SUCCESS,
+  LOGIN_FAIL,
+  LOGOUT,
+  CLEAR_ERRORS,
+} from '../types';
+
+const AuthState = (props) => {
+  const initialState = {
+    token: localStorage.getItem('token'),
+    isAuthenticated: false,
+    loading: true,
+    user: null,
+    error: null,
+  };
+
+  const [state, dispatch] = useReducer(authReducer, initialState);
+
+  // Load User
+
+  // Register User
+
+  // Login User
+
+  // Logout
+
+  // Clear Errors
+
+  return (
+    <AuthContext.Provider
+      value={{
+        token: state.token,
+        isAuthenticated: state.isAuthenticated,
+        loading: state.loading,
+        user: state.user,
+        error: state.error,
+      }}
+    >
+      {props.children}
+    </AuthContext.Provider>
+  );
+};
+
+export default AuthState;
