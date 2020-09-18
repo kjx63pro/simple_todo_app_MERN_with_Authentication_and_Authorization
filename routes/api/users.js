024bae1fae4db4b4297fcd44fd37f5203eb6d5c6
kjx63pro <kjx63pro@gmail.com>
2020-09-18 13:15:08 +0900

    Create User Model and api/user route, and register new user with a hashed password

---
+++ routes/api/users.js
@@ -0,0 +1,39 @@
+const express = require('express');
+const router = express.Router();
+const bcrypt = require('bcryptjs');
+
+const User = require('../../models/User');
+
+// @route     POST api/users
+// @desc      Register new user
+// @access    Public
+router.post('/', async (req, res) => {
+  const { name, email, password } = req.body;
+
+  if (!name || !email || !password) {
+    return res.status(400).json({ msg: 'Please enter all fields' });
+  }
+
+  try {
+    let user = await User.findOne({ email });
+    if (user) {
+      return res.status(400).json({ msg: 'User already exists' });
+    }
+
+    user = new User({
+      name,
+      email,
+      password,
+    });
+
+    const salt = await bcrypt.genSalt(10);
+    user.password = await bcrypt.hash(password, salt);
+
+    await user.save();
+    res.json(user);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).json({ msg });
+  }
+});
+module.exports = router;
--- server.js
+++ server.js
@@ -3,12 +3,12 @@ const connectDB = require('./config/db');
 
 const app = express();
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.json({ extended: false }));
 
 connectDB();
 // Define Routes
 app.use('/api/todos', require('./routes/api/todos'));
+app.use('/api/users', require('./routes/api/users'));
 
 // Production: PORT vs Development:5000
 const PORT = process.env.PORT || 5000;
